ce8800b0877cecbc7466471c53e69e3e2ab4330b
RichyRed <richyred1090@gmail.com>
2023-05-29 08:47:26 -0400

    commit1

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,50 +1,134 @@
 <template>
-  <footer class="bg-light py-3">
+  <footer class="footer">
     <div class="container">
       <div class="row">
         <div class="col-md-4">
-          <h4>{{ title }}</h4>
-          <p>{{ description }}</p>
+          <div class="footer-section">
+            <h4 class="footer-section-title">Enlaces útiles</h4>
+            <ul class="footer-links">
+              <li><a href="#">Inicio</a></li>
+              <li><a href="#">Acerca de nosotros</a></li>
+              <li><a href="#">Servicios</a></li>
+              <li><a href="#">Contacto</a></li>
+            </ul>
+          </div>
         </div>
         <div class="col-md-4">
-          <h4>{{ linksTitle }}</h4>
-          <ul class="list-unstyled">
-            <li><a href="#">Link 1</a></li>
-            <li><a href="#">Link 2</a></li>
-            <li><a href="#">Link 3</a></li>
-          </ul>
+          <div class="footer-section">
+            <h4 class="footer-section-title">Redes sociales</h4>
+            <div class="social-icons">
+              <a href="https://www.facebook.com/" class="social-icon" target="_blank">
+                <img src="../assets/faceLogo.png" alt="Facebook" class="icon-img" />
+              </a>
+              <a href="https://twitter.com/" class="social-icon" target="_blank">
+                <img src="../assets/twitterLogo.png" alt="Twitter" class="icon-img" />
+              </a>
+              <a href="https://www.instagram.com/" class="social-icon" target="_blank">
+                <img src="../assets/insta.png" alt="Instagram" class="icon-img" />
+              </a>
+              <a href="https://www.tiktok.com/" class="social-icon" target="_blank">
+                <img src="../assets/tiktokFondo.png" alt="TikTok" class="icon-img" />
+              </a>
+            </div>
+          </div>
         </div>
         <div class="col-md-4">
-          <h4>{{ contactTitle }}</h4>
-          <address>
-            <strong>{{ address }}</strong
-            ><br />
-            {{ city }}, {{ state }} {{ zip }}<br />
-            <abbr title="Phone">P:</abbr> {{ phone }}<br />
-            <a href="mailto:{{ email }}">{{ email }}</a>
-          </address>
+          <div class="footer-section">
+            <h4 class="footer-section-title">Información de contacto</h4>
+            <ul class="footer-links">
+              <li>Dirección: Wall Street</li>
+              <li>Teléfono: 123456789</li>
+              <li>Email: locangas@gmail.com</li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
   </footer>
 </template>
 
+
 <script>
 export default {
-  name: "FooteR",
-  data() {
-    return {
-      title: "Footer Title",
-      description: "Footer description",
-      linksTitle: "Links",
-      contactTitle: "Contact Us",
-      address: "1234 Main St.",
-      city: "Anytown",
-      state: "CA",
-      zip: "12345",
-      phone: "555-555-5555",
-      email: "email@example.com",
-    };
-  },
+  name: 'FooteR',
 };
 </script>
+
+<style>
+.footer {
+  background-color: #888888;
+  padding: 30px 0;
+  color: #fff;
+}
+
+.footer .container {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+
+.footer .row {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+
+.footer-section {
+  margin-bottom: 30px;
+  text-align: center;
+}
+
+.footer-section-title {
+  color: #fff;
+  font-size: 18px;
+  font-weight: bold;
+  margin-bottom: 10px;
+}
+
+.footer-links {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+.footer-links li {
+  margin-bottom: 5px;
+}
+
+.footer-links a {
+  color: #fff;
+  text-decoration: none;
+  transition: color 0.3s, border-bottom-color 0.3s;
+}
+
+.footer-links a:hover,
+.footer-links a:focus {
+  color: #000;
+  border-bottom: 1px solid #000;
+}
+
+.social-icons {
+  display: flex;
+  justify-content: center;
+}
+
+.social-icon {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: #fff;
+  margin: 5px;
+  transition: transform 0.3s;
+}
+
+.icon-img {
+  width: 24px;
+  height: 24px;
+}
+
+.social-icon:hover {
+  transform: scale(1.2);
+}
+</style>
--- src/components/Informes.vue
+++ src/components/Informes.vue
@@ -1,34 +1,63 @@
 <template>
   <div>
-    <h1>{{ title }}</h1>
+    <h1 class="informes-title">{{ title }}</h1>
     <div class="row">
-      <div class="md-4">
-        <h2>{{ subtitle1 }}</h2>
-        <ul>
-          <li>Sample text 1</li>
-          <li>Sample text 2</li>
-          <li>Sample text 3</li>
-        </ul>
+      <div class="col-md-4">
+        <div class="informes-section">
+          <h2 class="informes-subtitle informes-left">{{ subtitle1 }}</h2>
+          <ul>
+            <li>Sample text 1</li>
+            <li>Sample text 2</li>
+            <li>Sample text 3</li>
+          </ul>
+        </div>
       </div>
-      <div class="md-4">
-        <h2>{{ subtitle2 }}</h2>
-        <ul>
-          <li>Sample text 4</li>
-          <li>Sample text 5</li>
-          <li>Sample text 6</li>
-        </ul>
+      <div class="col-md-4">
+        <div class="informes-section">
+          <h2 class="informes-subtitle informes-left">{{ subtitle2 }}</h2>
+          <ul>
+            <li>Sample text 4</li>
+            <li>Sample text 5</li>
+            <li>Sample text 6</li>
+          </ul>
+        </div>
       </div>
-      <div class="md-4">
-        <h2>{{ subtitle3 }}</h2>
-        <ul>
-          <li>Sample text 7</li>
-          <li>Sample text 8</li>
-          <li>Sample text 9</li>
-        </ul>
+      <div class="col-md-4">
+        <div class="informes-section">
+          <h2 class="informes-subtitle informes-left">{{ subtitle3 }}</h2>
+          <ul>
+            <li>Sample text 7</li>
+            <li>Sample text 8</li>
+            <li>Sample text 9</li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
 </template>
+
+<style>
+.informes-title {
+  text-align: center;
+  color: red;
+  font-size: 40px;
+}
+
+.informes-subtitle {
+  text-align: left;
+  color: #555;
+  font-size: 18px;
+  margin-top: 10px;
+}
+
+.informes-left {
+  margin-left: 20px;
+}
+
+.informes-section {
+  margin-bottom: 20px;
+}
+</style>
 
 <script>
 export default {
