c3ac037f47c24757707ad97884422e18973fbd82
RichyRed <richyred1090@gmail.com>
2023-05-29 09:17:01 -0400

    commit2

--- src/components/Informes.vue
+++ src/components/Informes.vue
@@ -1,37 +1,29 @@
 <template>
   <div>
     <h1 class="informes-title">{{ title }}</h1>
-    <div class="row">
-      <div class="col-md-4">
-        <div class="informes-section">
-          <h2 class="informes-subtitle informes-left">{{ subtitle1 }}</h2>
-          <ul>
-            <li>Sample text 1</li>
-            <li>Sample text 2</li>
-            <li>Sample text 3</li>
-          </ul>
-        </div>
-      </div>
-      <div class="col-md-4">
-        <div class="informes-section">
-          <h2 class="informes-subtitle informes-left">{{ subtitle2 }}</h2>
-          <ul>
-            <li>Sample text 4</li>
-            <li>Sample text 5</li>
-            <li>Sample text 6</li>
-          </ul>
-        </div>
-      </div>
-      <div class="col-md-4">
-        <div class="informes-section">
-          <h2 class="informes-subtitle informes-left">{{ subtitle3 }}</h2>
-          <ul>
-            <li>Sample text 7</li>
-            <li>Sample text 8</li>
-            <li>Sample text 9</li>
-          </ul>
-        </div>
-      </div>
+    <div class="informes-section">
+      <h2 class="informes-subtitle informes-left">{{ subtitle1 }}</h2>
+      <ul class="informes-list">
+        <li class="informes-list-item">Sample text 1</li>
+        <li class="informes-list-item">Sample text 2</li>
+        <li class="informes-list-item">Sample text 3</li>
+      </ul>
+    </div>
+    <div class="informes-section">
+      <h2 class="informes-subtitle informes-left">{{ subtitle2 }}</h2>
+      <ul class="informes-list">
+        <li class="informes-list-item">Sample text 4</li>
+        <li class="informes-list-item">Sample text 5</li>
+        <li class="informes-list-item">Sample text 6</li>
+      </ul>
+    </div>
+    <div class="informes-section">
+      <h2 class="informes-subtitle informes-left">{{ subtitle3 }}</h2>
+      <ul class="informes-list">
+        <li class="informes-list-item">Sample text 7</li>
+        <li class="informes-list-item">Sample text 8</li>
+        <li class="informes-list-item">Sample text 9</li>
+      </ul>
     </div>
   </div>
 </template>
@@ -40,22 +32,31 @@
 .informes-title {
   text-align: center;
   color: red;
-  font-size: 40px;
+  font-size: 42px;
+  margin-bottom: 20px;
 }
 
 .informes-subtitle {
   text-align: left;
-  color: #555;
-  font-size: 18px;
-  margin-top: 10px;
-}
-
-.informes-left {
-  margin-left: 20px;
+  color: red;
+  font-size: 24px;
+  margin-top: 30px;
+  margin-left: 30px;
 }
 
 .informes-section {
-  margin-bottom: 20px;
+  margin-bottom: 30px;
+}
+
+.informes-list {
+  list-style-type: none;
+  padding-left: 0;
+  margin-top: 10px;
+}
+
+.informes-list-item {
+  margin-left: 30px;
+  margin-bottom: 5px;
 }
 </style>
 
