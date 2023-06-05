5c4be21b03200afcd40f3bb42a42ee3b50d96dcf
RichyRed <richyred1090@gmail.com>
2023-06-04 23:37:03 -0400

    avisos

--- src/components/Informes.vue
+++ src/components/Informes.vue
@@ -4,25 +4,43 @@
     <div class="informes-section">
       <h2 class="informes-subtitle informes-left">{{ subtitle1 }}</h2>
       <ul class="informes-list">
-        <li class="informes-list-item">Sample text 1</li>
-        <li class="informes-list-item">Sample text 2</li>
-        <li class="informes-list-item">Sample text 3</li>
+        <li class="informes-list-item">
+          <a class="informes-link" href="https://example.com/document1.pdf">Convocatoria De Licitación Pública Para Contratación De Seguro Colectivo</a>
+        </li>
+        <li class="informes-list-item">
+          <a class="informes-link" href="https://example.com/document2.pdf">Condiciones Particulares - Seguro de desgravamen Hipotecario 2023-2026</a>
+        </li>
+        <li class="informes-list-item">
+          <a class="informes-link" href="https://example.com/document3.pdf">Resultados de la Licitación Pública del Seguro de desgravamen N°001/2023</a>
+        </li>
       </ul>
     </div>
     <div class="informes-section">
       <h2 class="informes-subtitle informes-left">{{ subtitle2 }}</h2>
       <ul class="informes-list">
-        <li class="informes-list-item">Sample text 4</li>
-        <li class="informes-list-item">Sample text 5</li>
-        <li class="informes-list-item">Sample text 6</li>
+        <li class="informes-list-item">
+          <a class="informes-link" href="https://example.com/document4.pdf">Calificación de Desempeño en RSE 2020</a>
+        </li>
+        <li class="informes-list-item">
+          <a class="informes-link" href="https://example.com/document5.pdf">Coeficiente de Adecuación Patrimonial (CAP) noviembre 2019</a>
+        </li>
+        <li class="informes-list-item">
+          <a class="informes-link" href="https://example.com/document6.pdf">Coeficiente de Adecuación Patrimonial (CAP) 2019 y 2020</a>
+        </li>
       </ul>
     </div>
     <div class="informes-section">
       <h2 class="informes-subtitle informes-left">{{ subtitle3 }}</h2>
       <ul class="informes-list">
-        <li class="informes-list-item">Sample text 7</li>
-        <li class="informes-list-item">Sample text 8</li>
-        <li class="informes-list-item">Sample text 9</li>
+        <li class="informes-list-item">
+          <a class="informes-link" href="https://example.com/document7.pdf">Seguro Plan Tranquilidad</a>
+        </li>
+        <li class="informes-list-item">
+          <a class="informes-link" href="https://example.com/document8.pdf">Seguro Vida + Salud Preventiva</a>
+        </li>
+        <li class="informes-list-item">
+          <a class="informes-link" href="https://example.com/document9.pdf">Seguro de Vida por Covid - 19</a>
+        </li>
       </ul>
     </div>
   </div>
@@ -58,6 +76,15 @@
   margin-left: 30px;
   margin-bottom: 5px;
 }
+
+.informes-link {
+  color: inherit;
+  text-decoration: none;
+}
+
+.informes-link:hover {
+  text-decoration: underline;
+}
 </style>
 
 <script>
@@ -66,9 +93,9 @@ export default {
   data() {
     return {
       title: "Avisos",
-      subtitle1: "Subtitle 1",
-      subtitle2: "Subtitle 2",
-      subtitle3: "Subtitle 3",
+      subtitle1: "CONVOCATORIA DE LICITACIÓN PÚBLICA PARA CONTRATACIÓN DE SEGURO COLECTIVO",
+      subtitle2: "INFORMACIÓN ADICIONAL",
+      subtitle3: "SEGUROS EN PRO MUJER",
     };
   },
 };
